c57a8f9fe6b1dab157c801536a85f7ac0708897b
Nikita2799 <filatovnikita27@gmail.com>
2024-03-15 21:45:26 +0100

    fix config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
       script: "./dist/server.js",
       watch: true,
       watch_delay: 1000,
-      ignore_watch: ["node_modules", ".git", "uploads"],
+      ignore_watch: ["node_modules", ".git", "uploads", "dist"],
     },
   ],
 };
