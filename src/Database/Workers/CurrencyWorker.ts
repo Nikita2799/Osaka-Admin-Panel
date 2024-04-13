5a75bb09d9d91f5e743481ed92f7161d3dd85946
Nikita2799 <filatovnikita27@gmail.com>
2024-04-13 19:06:02 +0200

    add currency worker

--- src/Database/Workers/CurrencyWorker.ts
+++ src/Database/Workers/CurrencyWorker.ts
@@ -8,4 +8,7 @@ export class CurrencyWorker {
   async update(params: any) {
     return await this.currency.update({ ...params }, { where: { id: 1 } });
   }
+  async get(params: any) {
+    return await this.currency.findOne({ where: { id: 1 }, raw: true });
+  }
 }
